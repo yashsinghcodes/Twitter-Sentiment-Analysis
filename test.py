6aa6c138976d76b9f608f235278c0e4f50dab352
Yash Singh <32360914+yashsinghcodes@users.noreply.github.com>
2021-04-22 19:47:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-from main import TSA
 
-twitter = TSA('x74NGOqMiVZpSEjjjGBTnI2dy','EbADf2QvpocUXElVTj4g8kPdeIJXcjVvKWl3ldZK9cibzZ0pn9','940635260185989125-vmqsOUavgUU5JIXcSIozMBBzDs1qjfp','bUl7POlLxxIXPMpNAAzdfJkSu6acYgfp8onwYzUm3BHsx')
-twitter.analysis('Apple')
